fe7887ed133dcbe2f83df016b07a5f326816baa5
DEKENI Toha <dekenitoha097@gmail.com>
2025-12-25 22:54:38 +0100

    reset app

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Deen life</Text>
     </View>
   );
 }
